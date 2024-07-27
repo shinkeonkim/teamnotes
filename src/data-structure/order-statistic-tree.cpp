736717c538dacd58ec96cd1bf693625ff421d077
shinkeonkim <singun11@kookmin.ac.kr>
2024-07-27 18:30:47 +0900

    chore: 코드 정리

--- src/data-structure/order-statistic-tree.cpp
+++ src/data-structure/order-statistic-tree.cpp
@@ -14,16 +14,8 @@ int main()
 {
     ordered_set X;
     for (int i = 1; i < 10; i += 2) X.insert(i); // 1 3 5 7 9
-
-    cout << boolalpha;
-    cout << *X.find_by_order(2) << endl; // 5
     cout << *X.find_by_order(4) << endl; // 9
-    cout << (X.end() == X.find_by_order(5)) << endl; // true
-
-    cout << X.order_of_key(-1) << endl; // 0
-    cout << X.order_of_key(1) << endl; // 0
-    cout << X.order_of_key(5) << endl; // 2
+    cout << X.order_of_key(5) << endl; // 2; 5보다 작은 수의 갯수
     X.erase(3);
-    cout << X.order_of_key(4) << endl; // 1
     for (int t : X) printf("%d ", t); // 1 5 7 9
 }
