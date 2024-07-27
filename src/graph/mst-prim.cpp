60dcb8ef23fe7d8d11c615a3c52bed0396d59de5
shinkeonkim <singun11@kookmin.ac.kr>
2024-07-27 17:55:58 +0900

    feat: Kruskal 대신에 Prim으로

---
+++ src/graph/mst-prim.cpp
@@ -0,0 +1,47 @@
+struct edge {
+  ll crt;
+  ll node, cost;
+};
+
+struct WGraph {
+    ll V; 
+    vector<edge> adj[MAX];
+    vector<ll> prev;
+    WGraph(ll V) : V{V} {}
+    void addEdge(ll s, ll e, ll cost) {
+        adj[s].push_back({s, e, cost});
+        adj[e].push_back({e, s, cost});
+    }
+    
+    ll prim(vector<edge> &selected) { // selected에 선택된 간선정보 vector 담김
+        selected.clear();
+    
+        vector<bool> added(V, false);
+        llv1 minWeight(V, INF), parent(V, -1);
+    
+        ll ret = 0;
+        minWeight[0] = parent[0] = 0;
+        for (int iter = 0; iter < V; iter++) {
+            int u = -1;
+            for (int v = 0; v < V; v++) {
+                if (!added[v] && (u == -1 || minWeight[u]>minWeight[v]))
+                    u = v;
+            }
+    
+            if (parent[u] != u)
+                selected.push_back({parent[u], u, minWeight[u]});
+    
+            ret += minWeight[u];
+            added[u] = true;
+
+            for1(0, sz(adj[u])) {
+                int v = adj[u][i].node, weight = adj[u][i].cost;
+                if (!added[v] && minWeight[v]>weight) {
+                    parent[v] = u;
+                    minWeight[v] = weight;
+                }
+            }
+        }
+        return ret;
+    }
+};
